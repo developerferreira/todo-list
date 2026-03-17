da570749c1a9a6e64d8828cbd30004237de36420
Cauã Ferreira <185573157+developerferreira@users.noreply.github.com>
2026-03-17 18:46:13 -0300

    fix: saving task status

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+const taskDone = JSON.parse(localStorage.getItem('taskDone') || '[]');
 
 // pegando as tarefas salvas no localStorage
 tasks.forEach(task => {
     createTask(task);
 });
+
+// verificando quais tarefas estão marcadas como feitas e atualizando a interface
+if (taskDone.length > 0) {
+    taskDone.forEach(t => {
+        if (t.includes(' - done')) {
+            const taskElements = document.querySelectorAll('.task-text');
+
+            taskElements.forEach(el => {
+                if (el.textContent === t.replace(' - done', '')) {
+                    el.classList.add('task-text-done');
+                    const doneBtn = el.nextElementSibling.querySelector('.done-btn');
+                    doneBtn.innerHTML = '<i class="bi bi-x-lg"></i>';
+                }
+            });
+        }
+    });
+}
 
 function addTask() {
     const input = document.querySelector('#task-input');
@@ -64,7 +82,29 @@ function createTask(taskName) {
 
     doneBtn.addEventListener('click', () => {
         span.classList.toggle('task-text-done');
-        span.classList.contains('task-text-done') ? doneBtn.innerHTML = '<i class="bi bi-x-lg"></i>' : doneBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
+        span.classList.contains('task-text-done') 
+            ? doneBtn.innerHTML = '<i class="bi bi-x-lg"></i>' 
+            : doneBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
+
+        // atualizando o localStorage com o estado da tarefa (feita ou não)
+        const isTaskDone = span.classList.contains('task-text-done');
+        tasks.map(task => {
+            if (task === taskName) {
+                if (isTaskDone) {
+                    if (!taskDone.includes(task + ' - done')) {
+                        taskDone.push(task + ' - done');
+                    }
+                } else {
+                    const index = taskDone.indexOf(task + ' - done');
+
+                    if (index !== -1) {
+                        taskDone.splice(index, 1);
+                    }
+                }
+
+                localStorage.setItem('taskDone', JSON.stringify(taskDone));
+            }
+        })
     });
 }
 
@@ -76,10 +116,17 @@ function deleteTask(taskName) {
     // procurando a tarefa no localStorage e deletando ela
     const getTasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const newTasks = getTasks.filter(task => task !== taskName);
-
+    
+    // deletando as tarefas feitas também
+    const getTasksDone = JSON.parse(localStorage.getItem('taskDone')) || [];
+    const newTasksDone = getTasksDone.filter(task => task !== taskName + ' - done');
+    
     // atualizando o localStorage com as tarefas que sobraram
     localStorage.setItem('tasks', JSON.stringify(newTasks));
+    localStorage.setItem('taskDone', JSON.stringify(newTasksDone));
+
     tasks.splice(tasks.indexOf(taskName), 1);
+    taskDone.splice(taskDone.indexOf(taskName + ' - done'), 1);
 }
 
 // ação ao pressionar enter
